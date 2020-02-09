de4cb2274f4c61dc7f11362af372d560a32335ac
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2020-02-09 18:59:15 -0500

    agregado anotaciones entity mensajes de errores

--- src/main/java/com/eledi/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/eledi/springboot/backend/apirest/models/entity/Cliente.java
@@ -24,16 +24,16 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotEmpty
-	@Size(min = 4, max= 12)
+	@NotEmpty(message = "no puede estar vacio" )
+	@Size(min = 4, max= 12, message = "el tamaño tiene que estar entre 4 y 12")
 	@Column(nullable = false) //Para no permitir que el nombre pueda ser nulo
 	private String nombre;
 	
 	@NotEmpty
 	private String apellido;
 	
-	@NotEmpty
-	@Email
+	@NotEmpty(message = "no puede estar vacio" )
+	@Email(message = "no es una dirección de correo valida")
 	@Column(nullable = false, unique = true) 
 	private String email;
 	
